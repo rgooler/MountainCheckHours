03459895e7adb08e003e9eecc4c90eba07bb909e
Ryan Gooler <ryan.gooler@gmail.com>
2019-03-31 23:13:27 -0500

    Fix windows path issue

--- MountainCheckHours.py
+++ MountainCheckHours.py
@@ -1,7 +1,10 @@
 import numpy
+from os import path
 
 if __name__ == "__main__":
-    fname = "%UserProfile%\\AppData\\LocalLow\\David OReilly\\Mountain\\apsdfpboasdfhsa.sav"
+    fname = path.expandvars(r"%USERPROFILE%\\AppData\\LocalLow\\David OReilly\\Mountain\\apsdfpboasdfhsa.sav")
     a = numpy.fromfile(fname, dtype=int, count=-1, sep='')
     hours_done = a[11]
-    input("%s of 987 hours complete" % hours_done) 
+    print("%s of 987 hours complete" % hours_done) 
+    print("")
+    input("Press enter to exit program...")
