e94c3f5dbec78dc43cd9b4da87a1de5a0a67ad00
Ryan Gooler <ryan.gooler@gmail.com>
2019-04-02 19:19:39 -0500

    Check linux and current dir

--- MountainCheckHours.py
+++ MountainCheckHours.py
@@ -1,10 +1,34 @@
 import numpy
 from os import path
+import os
+
+savefile = "apsdfpboasdfhsa.sav"
+
+def find_savedir():
+    if path.isfile(savefile):
+        return '.'
+
+    winpath = path.expandvars(r"%USERPROFILE%\\AppData\\LocalLow\\David OReilly\\Mountain")
+    if path.isdir(winpath):
+        return winpath
+    
+    linpath = path.expandvars(r"~/.steam/steam/steamapps/compatdata/897330/pfx/dosdevices/z:/home")
+    linpath_x = "/.config/unity3d/David OReilly/Mountain"
+    if path.isdir(linpath):
+        for x in os.listdir(linpath):
+            if os.path.isdir(x):
+                fullpath = "%s/%s/%s" % (linpath, x, linpath_x)
+                if path.isdir(fullpath):
+                    return fullpath
 
 if __name__ == "__main__":
-    fname = path.expandvars(r"%USERPROFILE%\\AppData\\LocalLow\\David OReilly\\Mountain\\apsdfpboasdfhsa.sav")
-    a = numpy.fromfile(fname, dtype=int, count=-1, sep='')
-    hours_done = a[11]
-    print("%s of 987 hours complete" % hours_done) 
+    path = find_savedir()
+    if path is None:
+        print("Cannot find savegame. Please move this exe/script to your save folder for Mountain.")
+    else:
+        filename = "%s/%s" % (path, savefile)
+        a = numpy.fromfile(filename, dtype=int, count=-1, sep='')
+        hours_done = a[11]
+        print("%s of 987 hours complete" % hours_done) 
     print("")
     input("Press enter to exit program...")
